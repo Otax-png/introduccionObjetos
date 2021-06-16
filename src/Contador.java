12def89d8a8ddc3af2c10bd25e6fd9032a3316e3
Otax-png <taxturnil201@gmail.com>
2021-06-16 15:31:19 -0600

    Agregando Ejercicio1

---
+++ src/Contador.java
@@ -0,0 +1,30 @@
+package src;
+
+public class Contador{
+
+    private int contador;
+
+    public Contador(int contador){
+        this.contador = contador;
+    }
+
+    public int decremento(){
+        --contador;
+        return contador;
+    }
+
+    public int incremento(){
+        ++contador;
+        return contador;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+
+
+}
